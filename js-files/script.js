02b13358f0d73083bbcfc72524c5d4353cedb518
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2021-06-21 02:53:29 +0530

    debugging

--- js-files/script.js
+++ js-files/script.js
@@ -186,7 +186,8 @@ function handleSpace(){
             minusString = userType // Setting the string typed by user to minusString variable for future
         }
         else{
-            document.querySelector(".debug").innerText = `User Typed : ${usertype} \n Delete data : ${deleteData}\n Minus String : ${minusString}`
+            document.querySelector(".debug").innerText = `User Typed : ${userType} \n Delete data : ${deleteData}\n Minus String : ${minusString}`
+
             document.querySelector(".error-bundle").innerHTML = document.querySelector(".error-bundle").innerHTML +`<span class="error-word">${deleteData}</span>`
             document.querySelector(".type-area").value = minusString
             mistakeCount+=1
@@ -246,8 +247,7 @@ document.querySelector(".type-area").addEventListener('keydown', function (event
 document.querySelector(".type-area").addEventListener('keydown', function (event) {
     if(event.key === "Backspace" || event.keyCode == 8){
         var userTyped = document.querySelector(".type-area").value
-        console.log("User Typed : " + userTyped)
-        console.log("Minus String : " + minusString)
+
         if(userTyped == minusString || minusString.includes(userTyped)){
             event.preventDefault();
             return false;
